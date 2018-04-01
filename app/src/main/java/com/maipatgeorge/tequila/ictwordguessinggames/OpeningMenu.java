c63e4a338e680bad477ae0ee0238afb436dacf4d
napatchol thaipanich <patorseing@gmail.com>
2018-04-02 05:54:39 +0700

    test logout
    Signed-off-by: napatcholthaipanich <napatcholthaipanich@patorseing.(none)>

--- app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/OpeningMenu.java
+++ app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/OpeningMenu.java
@@ -1,14 +1,22 @@
 package com.maipatgeorge.tequila.ictwordguessinggames;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -17,6 +25,9 @@ public class OpeningMenu extends AppCompatActivity {
     LoginButton loginButton;
     TextView textView;
     CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
+    ProfileTracker profileTracker;
+    Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +40,44 @@ public class OpeningMenu extends AppCompatActivity {
 
         loginButton = (LoginButton)findViewById(R.id.fd_login_bn);
         textView = (TextView)findViewById(R.id.result);
+        logOut = (Button) findViewById(R.id.out);
         callbackManager = CallbackManager.Factory.create();
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                nextActivity(currentProfile);
+            }
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                logOut.setVisibility(View.GONE);
+                Profile profile = Profile.getCurrentProfile();
+                nextActivity(profile);
+                Toast.makeText(getApplicationContext(), "Loggin in ..." , Toast.LENGTH_SHORT).show();
                 textView.setText("Login Success \n" + loginResult.getAccessToken().getUserId()+"\n"+loginResult.getAccessToken().getToken());
+                //visibility
+                logOut.setVisibility(View.VISIBLE);
+                logOut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        LoginManager.getInstance().logOut();
+                        textView.setText("Logout");
+                        logOut.setVisibility(View.GONE);
+                    }
+                });
             }
 
             @Override
@@ -52,5 +95,29 @@ public class OpeningMenu extends AppCompatActivity {
     @Override
     protected void onActivityResult (int requestCode, int resultCode, Intent data){
         callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        nextActivity(profile);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    protected void nextActivity(Profile profile){
+        if (profile != null){
+            LoginManager.getInstance().logOut();
+        }
     }
 }
