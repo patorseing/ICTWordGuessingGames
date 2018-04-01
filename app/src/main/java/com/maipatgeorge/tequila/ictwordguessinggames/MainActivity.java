8dd5cf93e82967b10d5095a492c570e2807f8bb7
napatcholthaipanich <napatcholthaipanich@patorseing.(none)>
2018-04-01 22:17:53 +0700

    animation from 1st to 2nd

--- app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/MainActivity.java
+++ app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/MainActivity.java
@@ -1,13 +1,25 @@
 package com.maipatgeorge.tequila.ictwordguessinggames;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static int Welcome_Timeout = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(MainActivity.this, OpeningMenu.class);
+                startActivity(welcome);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, Welcome_Timeout);
     }
 }
