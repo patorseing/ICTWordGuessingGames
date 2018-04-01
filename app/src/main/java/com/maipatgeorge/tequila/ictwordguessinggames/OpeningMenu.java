655cc9296681e64f10b99220507ae343f1247219
napatcholthaipanich <napatcholthaipanich@patorseing.(none)>
2018-04-02 05:54:39 +0700

    login success but not link to next

--- app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/OpeningMenu.java
+++ app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/OpeningMenu.java
@@ -1,13 +1,56 @@
 package com.maipatgeorge.tequila.ictwordguessinggames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class OpeningMenu extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.getApplicationContext();
+
         setContentView(R.layout.activity_opening_menu);
+
+        loginButton = (LoginButton)findViewById(R.id.fd_login_bn);
+        textView = (TextView)findViewById(R.id.result);
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Success \n" + loginResult.getAccessToken().getUserId()+"\n"+loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 }
