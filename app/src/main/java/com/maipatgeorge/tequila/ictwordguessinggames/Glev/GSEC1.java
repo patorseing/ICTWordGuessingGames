d553eb8247cf54edb04d68c16e207cbf479b0303
napatcholthaipanich <napatcholthaipanich@patorseing.(none)>
2018-04-19 20:13:02 +0700

    update ref

---
+++ app/src/main/java/com/maipatgeorge/tequila/ictwordguessinggames/Glev/GSEC1.java
@@ -0,0 +1,15 @@
+package com.maipatgeorge.tequila.ictwordguessinggames.Glev;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.maipatgeorge.tequila.ictwordguessinggames.R;
+
+public class GSEC1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gsec1);
+    }
+}
